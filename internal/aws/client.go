0ef79ca6e9895ea5326b2d4a77d785554675b6e3
Harshwagh21 <harshwagh8411@gmail.com>
2026-03-22 01:51:07 +0530

    Added Aws Packages and wrote client for localstack aws where i added endpoint and region with test key id's of accesskeys

---
+++ internal/aws/client.go
@@ -0,0 +1,33 @@
+package aws
+
+import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
+func LocalStackConfig(ctx context.Context) (aws.Config, error){
+	cfg, err := config.LoadDefaultConfig(ctx,
+		config.WithRegion("ap-south-1"),
+		config.WithEndpointResolverWithOptions(
+			aws.EndpointResolverWithOptionsFunc(
+				func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+					return aws.Endpoint{
+						URL:               "http://localhost:4566",
+						HostnameImmutable: true,
+					}, nil
+				},
+			),
+		),
+		config.WithCredentialsProvider(
+			aws.CredentialsProviderFunc(func(ctx context.Context) (aws.Credentials, error) {
+				return aws.Credentials{
+					AccessKeyID:     "test",
+					SecretAccessKey: "test",
+				}, nil
+			}),
+		),
+	)
+
+	return cfg, err
+}
